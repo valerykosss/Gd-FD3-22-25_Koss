981dc80f8493c0633e4db4c0ea06a0bdf9b17b50
valerykosss <valery,kosss@gmail.com>
2025-01-27 12:44:14 +0300

    global packages deleted

--- ishop/src/reportWebVitals.ts
+++ ishop/src/reportWebVitals.ts
@@ -1,14 +1,11 @@
-import { ReportHandler } from 'web-vitals';
+import { onCLS, onFID, onLCP } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    // Используем новые функции из web-vitals
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onLCP(onPerfEntry);
   }
 };
 
