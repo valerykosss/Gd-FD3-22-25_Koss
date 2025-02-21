f7ff123a81432fe1528fba8b189563f6bde2303b
valerykosss <valery,kosss@gmail.com>
2025-02-21 16:09:47 +0300

    lesson 1: first react app, js repeated

---
+++ lections/15.01/script.js
@@ -0,0 +1,40 @@
+// init
+const DELAY_MS = 500;
+var variable = 1;
+let testName = 10;
+
+// if 
+// if () { 
+// } else if () {
+
+// } else {}
+
+//inf loop + break
+for (;;) {
+    break;
+}
+
+//не меняет массив
+[1, 2, 3].forEach(element => {})
+
+//возвращает новый массив
+[1, 2, 3].map(element => {})
+
+//true
+Boolean([])
+
+//true
+Boolean([].length)
+
+Array.from()
+[].map()
+[].forEach()
+[].filter()
+[].find()
+reduce
+
+rest - запаковывает 
+spread - распаковывает 
+
+JSON.parse
+JSON.stringify
